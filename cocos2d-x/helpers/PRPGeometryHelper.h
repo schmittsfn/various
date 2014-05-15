7ee7cca1cc3ef7bd8dc1306e0394062a4624e7f7
sts2055 <sts2055@gmail.com>
2014-05-15 17:43:40 +0200

    add file

---
+++ cocos2d-x/helpers/PRPGeometryHelper.h
@@ -0,0 +1,41 @@
+//
+//  PRPGeometryHelper.h
+//  primaryp
+//
+//  Created by stefan on 5/15/14.
+//
+//
+
+#ifndef primaryp_PRPGeometryHelper_h
+#define primaryp_PRPGeometryHelper_h
+
+/** Returns the angle of the line between two points */
+float getAngleOfLineBetweenTwoPoints(const cocos2d::Point& p1, const cocos2d::Point& p2)
+{
+    float xDiff = p2.x - p1.x;
+    float yDiff = p2.y - p1.y;
+    return atan2(yDiff, xDiff) * (180. / M_PI);
+}
+
+/** Computes the length of a vector */
+float getVectorLength(const cocos2d::Point& a)
+{
+	return sqrt(a.dot(a));
+}
+
+/** Returns the sign of the crossproduct as an integer with the value 1 */
+int getCrossProductSign(const cocos2d::Point& a, const cocos2d::Point& b)
+{
+	return (a.cross(b) < 0.) ? -1 : 1;
+}
+
+/** Returns the angle between two vectors */
+float getAngleBetweenVectors(const cocos2d::Point& a, const cocos2d::Point& b)
+{
+	const float cosA = a.dot(b) / (getVectorLength(a) * getVectorLength(b));
+	const float rad = acos(cosA);
+	float deg = CC_RADIANS_TO_DEGREES(rad);
+	return deg * getCrossProductSign(a, b);
+}
+
+#endif
